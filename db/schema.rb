d7ca07b0ea665bb2e7ffc3e1c0d82333072cdf72
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-02 15:55:25 +0200

    Wut?

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_134230) do
+ActiveRecord::Schema.define(version: 2018_08_02_082223) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "remember_digest"
   end
 
 end
