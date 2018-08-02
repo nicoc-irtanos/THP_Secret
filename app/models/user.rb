b34ed33218ce53991889d16bcc98fcd9f1a9b785
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-02 19:32:56 +0200

    bc de cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: {message: "Rentrez une adresse email valide de moins de 255 caractères."}, length: { maximum: 255 },
@@ -9,4 +10,31 @@ class User < ApplicationRecord
   validates :password, presence: true, 
                        confirmation: {message: "Les mots de passe ne correspondent pas"},
                        length: { in: 6..40, message: "Rentrez un mot de passe entre 6 et 40 caractères" }
+  
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
