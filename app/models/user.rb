00066959d47179626ed6232407bea780bd12bac1
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-02 16:56:48 +0200

    Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: {message: "Rentrez une adresse email valide de moins de 255 caractères"}, length: { maximum: 255 },
+  validates :email, presence: {message: "Rentrez une adresse email valide de moins de 255 caractères."}, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   has_secure_password validations: false
-  validates :password, presence: {message: "Rentrez un mot de passe entre 6 et 40 caractères"},
+  validates :password, presence: true
                        confirmation: true,
-                       length: { within: 6..40 }
-
-
+                       length: { within: 6..40, message: "Rentrez un mot de passe entre 6 et 40 caractères" }
 end
