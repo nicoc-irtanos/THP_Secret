5c8e8e857b0ee0f6f6187cccd4e4c05735ff7fa3
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-02 18:37:36 +0200

    forms qui marche

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help', as: 'help'
   get '/about', to: 'static_pages#about', as: 'about'
   get '/contact', to: 'static_pages#contact', as: 'contact'
-
+  
   resources :users
 
   get '/login', to: 'sessions#new'
