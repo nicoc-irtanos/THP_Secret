6be01a0bf39e84a1b142b2330ce822f675772249
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-02 16:04:39 +0200

    password et pas password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(email: params[:email].downcase,password_digest: params[:password])
-    redirect_to("/users/" + @user.id.to_s)
+    @user = User.create!(email: params[:email].downcase, password: params[:password])
     flash.now[:success] = "Profil utilisateur créé"
+    redirect_to("/users/" + @user.id.to_s)
   end
 
   def edit
