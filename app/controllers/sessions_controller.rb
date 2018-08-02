2e3a6abed3d681199ec05daae2f83ee059f02505
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-02 17:26:39 +0200

    Confirmation déco

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    redirect_to root_url, success: "Déconnecté !"
+    redirect_to root_url, flash: { success: "Déconnecté !" }
   end
 end
